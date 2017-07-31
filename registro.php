6786405546fb1466fe64274fc6cee7522791b2a5
bortan01 <brmiranda009@gmail.com>
2017-07-31 23:42:30 +0200

    Agregando proyecto
    Subiendo proyecto de tutoriales de JavaDevOne

---
+++ registro.php
@@ -0,0 +1,7 @@
+<?php
+include_once './app/Conexion.inc.php';
+include_once './app/RepositorioUsuario.inc.php';
+include_once './plantilla/documento-declaracion.inc.php';
+include './plantilla/barra.inc.php';
+include_once './plantilla/documento-cierre.inc.php';
+?>
