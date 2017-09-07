b67efed05450d109e67e33e7e4daf3f81e58d571
Bubba14016 <cartorres.95@gmail.com>
2017-09-07 10:47:50 -0600

    arreglar cabeceras

--- Final/plantillas/cabecera.php
+++ Final/plantillas/cabecera.php
@@ -8,8 +8,9 @@
         </title>
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-        <link href="../css/bootstrap.min.css" rel="stylesheet">
+        
         <link href="../css/materialize.min.css" rel="stylesheet">
+        <link href="../css/bootstrap.min.css" rel="stylesheet">
         <link href="../css/estilos.css" rel="stylesheet">
         <link rel="stylesheet" href="../css/font-awesome.min.css">
         <link href="../css/foto.css" rel="stylesheet">
