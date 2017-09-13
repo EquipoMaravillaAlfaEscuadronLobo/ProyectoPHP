0bf4b578ec1d6ef76906d7a3deb0a78f487859e0
bortan01 <brmiranda009@gmail.com>
2017-09-13 11:20:04 -0600

    mas validaciones

--- Final/usuario/registro_usuario.php
+++ Final/usuario/registro_usuario.php
@@ -1,5 +1,3 @@
-
-
 <!--formulario usuario-->
 <div class="container">
     <form id="FORMULARIO" method="post" class="form-horizontal" action="">
@@ -18,27 +16,25 @@
                         <div class="col m1"></div>
                         <div class="input-field col m5">
                             <i class="Medium material-icons prefix">account_circle</i> 
-                            <input type="text" id="idNombre" name="nameNombre"  class="text-center validate" maxlength="10" minlength="3" required>
+                            <input type="text" id="idNombre" name="nameNombre"  class="text-center validate" maxlength="25" minlength="3" required>
                             <label for="idNombre" class="col-sm-4 control-labe">Nombre <small> (Ej: Nombre1 Nombre2)</small></label>
                         </div>
                         <div class="input-field col m5">
                             <i class="material-icons prefix">account_circle</i> 
-                            <input type="text" id="idNombre" name="nameNombre"  class="text-center">
-                            <label for="idNombre">Apellido <small>(Ej: Apellido1 Apellido2)</small></label>
+                            <input type="text" id="idNombre" name="nameApellido"  class="text-center validate" maxlength="25" minlength="3" required>
+                            <label for="nameApellido">Apellido <small>(Ej: Apellido1 Apellido2)</small></label>
                         </div>
                     </div>
-
-
                     <div class="row">
                         <div class="col m1"></div>
                         <div class="input-field col m5">
                             <i class="material-icons prefix">local_phone</i> 
-                            <input type="text" id="idTelefono" name="nameTelefono" class="text-center">
+                            <input type="text" id="idTelefono" name="nameTelefono" class="text-center" required="">
                             <label for="idTelefono">Numero Telefonico <small>(Ej: 2255-5555)</small></label>
                         </div>
                         <div class="input-field col m5">
                             <i class="material-icons prefix">mail</i> 
-                            <input type="email" id="idEmail" name="nameEmail" class="text-center">
+                            <input type="email" id="idEmail" name="nameEmail" class="text-center validate" required="">
                             <label for="idEmail">Email <small>(Ej: correo@gmail.com)</small> </label>
                         </div>
                     </div>
@@ -46,25 +42,24 @@
                         <div class="col m1"></div>
                         <div class="input-field col m5">
                             <i class="material-icons prefix">location_on</i> 
-                            <input type="text" id="idEmail" name="nameEmail" class="text-center">
-                            <label for="idEmail">Direccion <small>(Ej: Verapaz, Colonia Mercenenes)</small> </label>
+                            <input type="text" id="idEmail" name="nameDireccion" class="text-center validate" minlength="10" required="">
+                            <label for="nameDireccion">Direccion <small>(Ej: Verapaz, Colonia Mercenenes)</small> </label>
                         </div>
 
                         <div class="input-field col m1">
                             <div class="input-field col m1">
                                 <i class="material-icons prefix">location_city</i>     
                             </div>
-
                         </div>
-
                         <div class="input-field col m4">
-
-                            <select>
-
+                            <select required="">
                                 <option value = "" disabled selected>Seleccione Institucion</option>
                                 <option value = "1">Centro Escolar Presbitero Norberto Marroquín</option>
                                 <option value = "2">Instituto Nacional de San José Verapaz</option>
                                 <option value = "3">Instituto Nacional Dr. Sarvelio Navarrete</option>
+                                <option value = "4">Escuela católica, San Jose Verapaz</option>
+                                <option value = "5">Alcaldía Municial, San José Verapaz</option>
+                                <option value = "6">Otros</option>
                             </select>
                         </div>
                     </div>
@@ -132,11 +127,17 @@
 
         $("#FORMULARIO").validate({
             rules: {
-                nameNombre: "required",
-                lastname1: "required",
-                username1: {
+                nameNombre: {
                     required: true,
-                    minlength: 2
+                    minlength: 3
+                },
+                nameApellido: {
+                     required: true,
+                    minlength: 3
+                },
+                nameDireccion: {
+                    required: true,
+                    minlength: 10
                 },
                 password1: {
                     required: true,
@@ -147,18 +148,24 @@
                     minlength: 5,
                     equalTo: "#password1"
                 },
-                email1: {
+                nameEmail: {
                     required: true,
                     email: true
                 },
                 agree1: "required"
             },
             messages: {
-                nameNombre: "por favor ingrese su primer nombre",
-                lastname1: "Please enter your lastname",
-                username1: {
-                    required: "Please enter a username",
-                    minlength: "Your username must consist of at least 2 characters"
+                nameNombre: {
+                   required: "Por favor ingrese su Nombre",
+                   minlength: "El nombre debe de tener por lo menos 3 caracteres" 
+                },
+                nameApellido: {
+                   required: "Por favor ingrese su Apellido",
+                   minlength: "El apellido debe de tener por lo menos 3 caracteres" 
+                },
+                nameDireccion: {
+                    required: "Por favor ingrese la direccion",
+                    minlength: "ingrese una direccion real"
                 },
                 password1: {
                     required: "Please provide a password",
@@ -169,7 +176,7 @@
                     minlength: "Your password must be at least 5 characters long",
                     equalTo: "Please enter the same password as above"
                 },
-                email1: "Please enter a valid email address",
+                email1: "Por favor ingrese un correo valido",
                 agree1: "Please accept our policy"
             },
             errorElement: "em",
