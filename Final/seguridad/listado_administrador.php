9d92fbbbb4926313240276947eb3cca91a2621cb
bortan01 <brmiranda009@gmail.com>
2017-09-21 12:26:23 -0600

    foto y botones arreglado

--- Final/seguridad/listado_administrador.php
+++ Final/seguridad/listado_administrador.php
@@ -11,33 +11,33 @@
 
 
             <div class="panel-body">
-                <table padding="20px" class="responsive-table display" id="data-table-simple">
+                <table padding="20px" class="responsive-table table-sm display" id="data-table-simple">
                     <thead class="">
-                    <th class="text-center">Modificar</th>
+                    <th class="text-center"></th>
                     <th class="text-center">Nombre Completo</th>
                     <th class="text-center">Usuario</th>
                     <th class="text-center">Nivel</th>
                     <th class="text-center">Foto</th>
-                    <th class="text-center">Eliminar</th>
+                    <th class="text-center"></th>
 
                     </thead>
                     <tbody>
                         <tr>
-                            <td class="text-center"><button class="btn btn-success btn-block btn-large" onclick="abrirEdicion()"> <i class="Medium material-icons prefix">edit</i> </button></td>
+                            <td class="text-center"><button class="btn btn-success" onclick="abrirEdicion()"> <i class="Medium material-icons prefix">edit</i> </button></td>
                             <td class="text-center">jhon Cefhdsk hfkadhfkj fhafkjahkjna</td>
                             <td class="text-center">jhon01</td>
                             <td class="text-center">Admin</td>
                             <td class="text-center"><img src="../imagenes/imagenes.jpg" class="presentacionXZ" alt=""></td>
-                            <td class="text-center"><button class="btn btn-danger btn-block btn-large" onclick="abrirEdicion()"> <i class="Medium material-icons prefix">delete</i> </button></td>
+                            <td class="text-center"><button class="btn btn-danger" onclick="abrirEdicion()"> <i class="Medium material-icons prefix">delete</i> </button></td>
                         </tr>
 
                         <tr>
-                            <td class="text-center"><button class="btn btn-success btn-block btn-large"> <i class="Medium material-icons prefix">edit</i> </button></td>
+                            <td class="text-center"><button class="btn btn-success "> <i class="Medium material-icons prefix">edit</i> </button></td>
                             <td class="text-center">albertano Cefhdsk hfkadhfkj fhafkjahkjna</td>
                             <td class="text-center">albert 02</td>
                             <td class="text-center">root </td>
                             <td class="text-center"><img src="../imagenes/imagenes.jpg" class="presentacionXZ" alt=""></td>
-                            <td class="text-center"><button class="btn btn-danger btn-block btn-large"> <i class="Medium material-icons prefix">delete</i> </button></td>
+                            <td class="text-center"><button class="btn btn-danger"> <i class="Medium material-icons prefix">delete</i> </button></td>
                         </tr>
                     </tbody>
                 </table>
