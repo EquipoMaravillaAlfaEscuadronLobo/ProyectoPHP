006237947b6eab64cc054530f8d9169622538e5d
Bubba14016 <cartorres.95@gmail.com>
2017-10-01 16:13:39 -0600

    listados biblioteca

---
+++ SICAPL/biblioteca/listad_lib.php
@@ -0,0 +1,52 @@
+
+<table padding="20px" class="responsive-table display" id="data-table-simple">
+                    <thead class="">
+                    <th class="text-center"></th>
+                    <th class="text-center">Titulo</th>
+                    <th class="text-center">Autores</th>
+                    <th class="text-center">Editorial</th>
+                    <th class="text-center">Cantidad</th>
+                    
+                    <th class="text-center"></th>
+
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="text-center"><button class="btn btn-success" onclick="abrirEdicion()"> <i class="Medium material-icons prefix">edit</i> </button></td>
+                            <td class="text-center">Papirusa</td>
+                            <td class="text-center">Hugo Aguirre</td>
+                            <td class="text-center">Harday Electric</td>
+                            <td class="text-center">4</td>
+                            
+                            <td class="text-center"><button class="btn btn-danger"> <i class="Medium material-icons prefix">delete</i> </button></td>
+                        </tr>
+                        
+                        <tr>
+                            <td class="text-center"><button class="btn btn-success" onclick="abrirEdicion()"> <i class="Medium material-icons prefix">edit</i> </button></td>
+                            <td class="text-center">Iliada</td>
+                            <td class="text-center">Homero</td>
+                            <td class="text-center">Anaya</td>
+                            <td class="text-center"> 3</td>
+                            
+                            <td class="text-center"><button class="btn btn-danger"> <i class="Medium material-icons prefix">delete</i> </button></td>
+                        </tr>
+                        <tr>
+                            <td class="text-center"><button class="btn btn-success" onclick="abrirEdicion()"> <i class="Medium material-icons prefix">edit</i> </button></td>
+                            <td class="text-center">Odisea</td>
+                            <td class="text-center">Homero</td>
+                            <td class="text-center">Anaya</td>
+                            <td class="text-center">3</td>
+                            
+                            <td class="text-center"><button class="btn btn-danger"> <i class="Medium material-icons prefix">delete</i> </button></td>
+                        </tr>
+                        <tr>
+                            <td class="text-center"><button class="btn btn-success" onclick="abrirEdicion()"> <i class="Medium material-icons prefix">edit</i> </button></td>
+                            <td class="text-center">Cuentos de Barro</td>
+                            <td class="text-center">Salarrue</td>
+                            <td class="text-center">Oceano</td>
+                            <td class="text-center">2</td>
+                            
+                            <td class="text-center"><button class="btn btn-danger"> <i class="Medium material-icons prefix">delete</i> </button></td>
+                        </tr>
+                    </tbody>
+                </table>
