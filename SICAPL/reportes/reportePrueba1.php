cc770ba56d5d2cb8d70ff565b634e28b292b6413
Bubba14016 <cartorres.95@gmail.com>
2017-12-19 23:07:18 -0600

    probando reportes

---
+++ SICAPL/reportes/reportePrueba1.php
@@ -0,0 +1,18 @@
+<?php
+    include_once '../class/tcpdf/tcpdf.php';
+    include_once '../class/PHPJasperXML-0.9d.inc.php';
+    include_once '../setting.php';
+    
+    $PHPJasperXML = new PHPJasperXML();
+    //$PHPJasperXML ->
+    $PHPJasperXML ->load_xml_file('librosBarCode.jrxml');
+    $PHPJasperXML ->transferDBtoArray($server, $user, $pass, $db);   
+    $PHPJasperXML ->outpage("I");
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+
