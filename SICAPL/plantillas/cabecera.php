1a6b3f54e351384a29d58c45724c2ffb37375525
Bubba14016 <cartorres.95@gmail.com>
2018-01-01 00:40:41 -0600

    arreglar redireccion

--- SICAPL/plantillas/cabecera.php
+++ SICAPL/plantillas/cabecera.php
@@ -2,11 +2,8 @@
 session_start();
     if (!isset($_SESSION['user'])&&($titulo1!="Inicio de Sesion"&&$titulo1!="Recuperar Contraseña")) {
         header("Location: ../index.php");
-    }else{
-        if (isset($_SESSION['user'])&&($titulo1!="Inicio de Sesion"&&$titulo1!="Recuperar Contraseña")) {
-        header("Location: home.php");
     }
-    }
+    
 
 ?>
 <!DOCTYPE html>
