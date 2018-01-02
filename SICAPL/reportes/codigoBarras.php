20c23a11fe293efa93dc3d05e4aaa59ad24340d1
Bubba14016 <cartorres.95@gmail.com>
2018-01-02 12:06:13 -0600

    nuevo de reportes

---
+++ SICAPL/reportes/codigoBarras.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Codigo de Barras Libros</title>
+</head>
+<body>
+	<img src="barcode.php?text=cej-002-034-pa-001-001&size=50&print=true">
+</body>
+</html>
