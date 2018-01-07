ff63d03b0f688b073bf9d2738ace4b803f75e98f
bortan01 <brmiranda009@gmail.com>
2018-01-07 12:35:20 -0600

    ya lo puse al lado

--- SICAPL/reportes/reporte_usuario/foto.php
+++ SICAPL/reportes/reporte_usuario/foto.php
@@ -27,164 +27,207 @@ $i = 1;
         <title>Document</title>
     </head>
     <style>
-        .tabla{
+    .tabla{
 
-            align-content: stretch;
-            width: 100%;
+        align-content: stretch;
+        width: 100%;
 
-        }
-        h1 {color: #000033 ;
-            text-align: center}
-        h2 {color: #000055;
-            text-align: center}
-        h3 {color: #000077; text-align: center}
-        .pie{
-            float: bottom;
-            margin-bottom: 0;
-            text-align: right;
-        }
-        table{
-            border-spacing: 10px;
+    }
+    h1 {color: #000033 ;
+        text-align: center}
+    h2 {color: #000055;
+        text-align: center}
+    h3 {color: #000077; text-align: center}
+    .pie{
+        float: bottom;
+        margin-bottom: 0;
+        text-align: right;
+    }
+    table{
+        border-spacing: 10px;
 
-            border-bottom: 1px;
+        border-bottom: 1px;
 
-        }
-        #titulo{
+    }
+    #titulo{
 
-        }
-        .titulo{
+    }
+    .titulo{
 
-            width: 600px !important;
-            text-align: left !important;
-        }
-        .fecha{
+        width: 600px !important;
+        text-align: left !important;
+    }
+    .fecha{
 
-            width: 100px !important;
-            text-align: right;
-        }
-        .espacio{
-            width: 110px;
-        }
+        width: 100px !important;
+        text-align: right;
+    }
+    .espacio{
+        width: 110px;
+    }
 
-        .tabla table td{
-            padding: 0;
-            margin: 0;
-            width: 300px;
-        }
-
-
-        .cabecera{
-            display: block;
-            width: 25%;
-            float: right;
-        }
-        .cabecera-izquierda{
-            display: block;
-            width: 25%;
-            float: left;
-        }
-        .cabecera h1{
-            text-align: center;
-        }
-        table.page_footer {width: 100%;
-                           border: none;
-                           background-color: #DDDDFF;
-                           border-top: solid 1mm #AAAADD;
-                           padding: 2mm;
-        }
-        table.page_header {width: 100%;
-                           border: none;
-                           background-color: #DDDDFF;
-                           border-bottom: solid 1mm #AAAADD;
-                           padding: 2mm; 
-        }
-        .portada{
-            padding-top: 500px;
-            float: bottom;
-            text-align: center;
-            width: 100%;
-        }
-        .frontal{
-            border-width: 1px;
-            border-style: dashed;
-            border-color: red;
-            margin: 10px;
-            width: 295px;
-            height: 195px;
-            background: #DDDDFF;
-
-        }
-        .encabezado{
-            text-align: center;
-            font-size: 14px;
-            font-weight: bold;
-            margin-bottom: 10px;
-        }
-        .foto{
-            margin-left:  0px;
-            height: 135px;
-            width: 110px;
-            margin-left:  10px;
-
-        }
-        .texto{
-
-            width: 160px;
-            height: 120px;
-            background: orange;
-            position: relative;
-            left: 4px;
-            top: -35px;
-
-        }
-        
-        
-        .ib{
-            display: inline-block;
-        }
-        p{
-            text-align: center;
-            font-size: 12px;
-        }
-        .datos{
-            width: 295px;
-            height: 100px;
-            text-align: center;
-            font-size: 12px;
-            background: #388E3C;
-        }
+    .tabla table td{
+        padding: 0;
+        margin: 0;
+        width: 300px;
+    }
 
 
-    </style>
+    .cabecera{
+        display: block;
+        width: 25%;
+        float: right;
+    }
+    .cabecera-izquierda{
+        display: block;
+        width: 25%;
+        float: left;
+    }
+    .cabecera h1{
+        text-align: center;
+    }
+    table.page_footer {width: 100%;
+                       border: none;
+                       background-color: #DDDDFF;
+                       border-top: solid 1mm #AAAADD;
+                       padding: 2mm;
+    }
+    table.page_header {width: 100%;
+                       border: none;
+                       background-color: #DDDDFF;
+                       border-bottom: solid 1mm #AAAADD;
+                       padding: 2mm; 
+    }
+    .portada{
+        padding-top: 500px;
+        float: bottom;
+        text-align: center;
+        width: 100%;
+    }
+
+    .contenedor{
+        border-width: 1px;
+        border-style: dashed;
+        border-color: red;
+        width: 900px;
+        height: 100px;
+        background: #DDDDFF;
+
+    }
+    .contenedor > div{
+        display: inline-block;
+    }
+
+    .frontal{
+        border-width: 1px;
+        border-style: dashed;
+        border-color: red;
+        margin: 10px;
+        width: 295px;
+        height: 195px;
+        margin-right: 0px;
+        background: #EEEE00; 
+    }
+    .trasero{
+        border-width: 1px;
+        border-style: dashed;
+        border-color: red;
+        margin: 10px;
+        width: 295px;
+        height: 195px;
+        margin-right: 0px;
+        background: #EEEE00;
+top: 0px;
+
+
+    }
+
+
+    .encabezado{
+        text-align: center;
+        font-size: 14px;
+        font-weight: bold;
+        margin-bottom: 10px;
+    }
+    .foto{
+        margin-left:  0px;
+        height: 135px;
+        width: 110px;
+        margin-left:  10px;
+
+    }
+    .texto{
+
+        width: 160px;
+        height: 120px;
+        background: #EEEE00;
+        position: relative;
+        left: 4px;
+        top: 0px;
+
+    }
+
+
+    .ib{
+        display: inline-block;
+    }
+    p{
+        text-align: center;
+        font-size: 12px;
+    }
+    .datos{
+        width: 295px;
+        height: 100px;
+        text-align: center;
+        font-size: 12px;
+        background: #388E3C;
+    }
+
+
+
+
+</style>
 
     <body>
     <page pageset="old"><!-- Etiqueta para cada pagina del reporte-->
 
-      <div class="frontal ib">
+    <div class="contenedor">
+
+        <div class="ib frontal">
             <p class="encabezado">CASA DE ENCUENTRO JUVENIL</p>
 
-           <img src="../../imagenes/hqdefault.jpg" class="foto ib" alt="">
+            <img src="../../imagenes/hqdefault.jpg" class="foto ib" alt="">
 
-            <div class="ib texto">
+            <div class="texto ib ">
                 <p> CARNET:  <strong>MA14049</strong></p> 
                 <p> NOMBRE:  <strong>BORIS RICARDO MIRANDA AYALA</strong></p> 
-                <p>FECHA EMISIÓN:  <strong><?php date_default_timezone_set('America/El_Salvador'); echo date('d/m/Y'); ?></strong> </p>
+                <p>FECHA EMISIÓN:  <strong><?php date_default_timezone_set('America/El_Salvador');
+echo date('d/m/Y');
+?></strong> </p>
             </div>
         </div>
-        <div class="frontal ib">
-            
-           <div class="ib datos">
+
+        <div class="trasero">
+
+            <div class="ib datos">
                 <p> TELÉFONO :  <strong>7777-7777</strong></p> 
                 <p> CORREO:  <strong>brmiranda009@gmail.com</strong></p> 
                 <p> DIRECCION :  <strong>Verapaz</strong> </p>
+                <p> <barcode dimension="1D"
+                             type="C128"
+                             value="cej-002-003-pa-003-002" 
+                             label="label"
+                             style="width:70%;
+                             height:15mm;
+                             color: black;
+                             font-size: 4mm">
+                </barcode> </p>
             </div>
         </div>
-        
-        
-             
+    </div>
 
 
 
-    </page>
+</page>
 </body>
 </html>
--- SICAPL/usuario/reportes/imprimir_carnet.php
+++ SICAPL/usuario/reportes/imprimir_carnet.php
@@ -21,189 +21,211 @@ $i = 1;
     -->
 </style>
 <style>
-        .tabla{
+    .tabla{
 
-            align-content: stretch;
-            width: 100%;
+        align-content: stretch;
+        width: 100%;
 
-        }
-        h1 {color: #000033 ;
-            text-align: center}
-        h2 {color: #000055;
-            text-align: center}
-        h3 {color: #000077; text-align: center}
-        .pie{
-            float: bottom;
-            margin-bottom: 0;
-            text-align: right;
-        }
-        table{
-            border-spacing: 10px;
+    }
+    h1 {color: #000033 ;
+        text-align: center}
+    h2 {color: #000055;
+        text-align: center}
+    h3 {color: #000077; text-align: center}
+    .pie{
+        float: bottom;
+        margin-bottom: 0;
+        text-align: right;
+    }
+    table{
+        border-spacing: 10px;
 
-            border-bottom: 1px;
+        border-bottom: 1px;
 
-        }
-        #titulo{
+    }
+    #titulo{
 
-        }
-        .titulo{
+    }
+    .titulo{
 
-            width: 600px !important;
-            text-align: left !important;
-        }
-        .fecha{
+        width: 600px !important;
+        text-align: left !important;
+    }
+    .fecha{
 
-            width: 100px !important;
-            text-align: right;
-        }
-        .espacio{
-            width: 110px;
-        }
+        width: 100px !important;
+        text-align: right;
+    }
+    .espacio{
+        width: 110px;
+    }
 
-        .tabla table td{
-            padding: 0;
-            margin: 0;
-            width: 300px;
-        }
+    .tabla table td{
+        padding: 0;
+        margin: 0;
+        width: 300px;
+    }
 
 
-        .cabecera{
-            display: block;
-            width: 25%;
-            float: right;
-        }
-        .cabecera-izquierda{
-            display: block;
-            width: 25%;
-            float: left;
-        }
-        .cabecera h1{
-            text-align: center;
-        }
-        table.page_footer {width: 100%;
-                           border: none;
-                           background-color: #DDDDFF;
-                           border-top: solid 1mm #AAAADD;
-                           padding: 2mm;
-        }
-        table.page_header {width: 100%;
-                           border: none;
-                           background-color: #DDDDFF;
-                           border-bottom: solid 1mm #AAAADD;
-                           padding: 2mm; 
-        }
-        .portada{
-            padding-top: 500px;
-            float: bottom;
-            text-align: center;
-            width: 100%;
-        }
-        .frontal{
-            border-width: 1px;
-            border-style: dashed;
-            border-color: red;
-            margin: 10px;
-            width: 295px;
-            height: 195px;
-            margin-right: 0px;
-            background: #EEEE00;
+    .cabecera{
+        display: block;
+        width: 25%;
+        float: right;
+    }
+    .cabecera-izquierda{
+        display: block;
+        width: 25%;
+        float: left;
+    }
+    .cabecera h1{
+        text-align: center;
+    }
+    table.page_footer {width: 100%;
+                       border: none;
+                       background-color: #DDDDFF;
+                       border-top: solid 1mm #AAAADD;
+                       padding: 2mm;
+    }
+    table.page_header {width: 100%;
+                       border: none;
+                       background-color: #DDDDFF;
+                       border-bottom: solid 1mm #AAAADD;
+                       padding: 2mm; 
+    }
+    .portada{
+        padding-top: 500px;
+        float: bottom;
+        text-align: center;
+        width: 100%;
+    }
 
-        }
-        .encabezado{
-            text-align: center;
-            font-size: 14px;
-            font-weight: bold;
-            margin-bottom: 10px;
-        }
-        .foto{
-            margin-left:  0px;
-            height: 135px;
-            width: 110px;
-            margin-left:  10px;
-
-        }
-        .texto{
-
-            width: 160px;
-            height: 120px;
-            background: #EEEE00;
-            position: relative;
-            left: 4px;
-            top: 0px;
-
-        }
+    .contenedor{
         
+        width: 750px;
+        height:200px;
         
-        .ib{
-            display: inline-block;
-        }
-        p{
-            text-align: center;
-            font-size: 12px;
-        }
-         .datos{
-            width: 295px;
-            height: 100px;
-            text-align: center;
-            font-size: 12px;
-            background: #388E3C;
-        }
+
+    }
+    .contenedor > div{
+        display: inline-block;
+    }
+
+    .frontal{
+        border-width: 1px;
+        border-style: dashed;
+        border-color: red;
+        margin: 10px;
+        width: 295px;
+        height: 195px;
+        margin-right: 0px;
+        background: #EEEE00; 
+    }
+    .trasero{
+        border-width: 1px;
+        border-style: dashed;
+        border-color: red;
+        margin: 10px;
+        width: 295px;
+        height: 195px;
+        margin-right: 0px;
+        background: #EEEE00;
+        position: absolute;
+        top: 20px;
+        left : 420px;
         
-         .contenedor{
-            border-width: 1px;
-            border-style: dashed;
-            border-color: red;
-            width: 750px;
-            height: 600px;
-            background: #DDDDFF;
-            
-        }
 
 
-    </style>
+
+    }
 
 
-    <page pageset="old"><!-- Etiqueta para cada pagina del reporte-->
-     
-        <div class=" ib contenedor">
-        
-            <div class="ib frontal">
+    .encabezado{
+        text-align: center;
+        font-size: 14px;
+        font-weight: bold;
+        margin-bottom: 10px;
+    }
+    .foto{
+        margin-left:  0px;
+        height: 135px;
+        width: 110px;
+        margin-left:  10px;
+
+    }
+    .texto{
+
+        width: 160px;
+        height: 120px;
+        background: #EEEE00;
+        position: relative;
+        left: 4px;
+        top: 0px;
+
+    }
+
+
+    .ib{
+        display: inline-block;
+    }
+    p{
+        text-align: center;
+        font-size: 12px;
+    }
+    .datos{
+        width: 295px;
+        height: 100px;
+        text-align: center;
+        font-size: 12px;
+        background: #388E3C;
+    }
+
+
+
+
+</style>
+
+
+<page pageset="old"><!-- Etiqueta para cada pagina del reporte-->
+
+    <div class="contenedor">
+
+        <div class="ib frontal">
             <p class="encabezado">CASA DE ENCUENTRO JUVENIL</p>
 
-           <img src="../../imagenes/hqdefault.jpg" class="foto ib" alt="">
+            <img src="../../imagenes/hqdefault.jpg" class="foto ib" alt="">
 
             <div class="texto ib ">
                 <p> CARNET:  <strong>MA14049</strong></p> 
                 <p> NOMBRE:  <strong>BORIS RICARDO MIRANDA AYALA</strong></p> 
-                <p>FECHA EMISIÓN:  <strong><?php date_default_timezone_set('America/El_Salvador'); echo date('d/m/Y'); ?></strong> </p>
+                <p>FECHA EMISIÓN:  <strong><?php date_default_timezone_set('America/El_Salvador');
+echo date('d/m/Y');
+?></strong> </p>
             </div>
         </div>
-     
-            <div class="frontal ib">
-            
-           <div class="ib datos">
+
+        <div class="trasero">
+
+            <div class="ib datos">
                 <p> TELÉFONO :  <strong>7777-7777</strong></p> 
                 <p> CORREO:  <strong>brmiranda009@gmail.com</strong></p> 
                 <p> DIRECCION :  <strong>Verapaz</strong> </p>
                 <p> <barcode dimension="1D"
-                            type="C128"
-                            value="cej-002-003-pa-003-002" 
-                            label="label"
-                            style="width:70%;
-                            height:15mm;
-                            color: black;
-                            font-size: 4mm">
-                            </barcode> </p>
+                             type="C128"
+                             value="cej-002-003-pa-003-002" 
+                             label="label"
+                             style="width:70%;
+                             height:15mm;
+                             color: black;
+                             font-size: 4mm">
+                </barcode> </p>
             </div>
         </div>
-        </div>
+    </div>
 
 
 
-    </page>
-    <?php
-
-
+</page>
+<?php
 $html = ob_get_clean();
 
 $html2pdf = new Html2Pdf('P', 'A4', 'es', 'true', 'UTF-8');
