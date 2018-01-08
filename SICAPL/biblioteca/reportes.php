0fbe02e84d60a0df6918ed332e86575df75e02d0
Bubba14016 <cartorres.95@gmail.com>
2018-01-08 08:39:45 -0600

    dos reportes mas

--- SICAPL/biblioteca/reportes.php
+++ SICAPL/biblioteca/reportes.php
@@ -1,7 +1,7 @@
 
     <a href="../reportes/LibrosDadosDeBaja.php" target='_blank'><p class="alert alert-info">Libros Dados de Baja</p></a>
     <a href="../reportes/LibrosDanados.php" target='_blank'><p class="alert alert-info">Libros Da&ntilde;ados</p></a>
-    <a href="../reportes/LibrosExtraviados.php" target='_blank'><p class="alert alert-info">Libros Extravados</p></a>
+    <a href="../reportes/LibrosExtraviados.php" target='_blank'><p class="alert alert-info">Libros Extraviados</p></a>
     <a href="../reportes/LibrosMasPrestados.php" target='_blank'><p class="alert alert-info">Libros m&aacute;s prestados</p></a>
     <a href="../reportes/CodigoBarra.php" target='_blank'><p class="alert alert-info">C&oacute;digos de Barra</p></a>
     
